317f03ef5c26950189a0dd379cfdd38fc1ca7b9d
philipkariuki <kariukiphil@gmail.com>
2019-08-04 02:31:20 +0300

    in views.py imports most of the required django modules creates forms.py in app

--- moringaawardz/views.py
+++ moringaawardz/views.py
@@ -1,4 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from .models import *
+from .forms import *
+from django.http  import HttpResponse, Http404, HttpResponseRedirect
+from registration.backends.simple.views import RegistrationView
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 def index(request):
